1321671359f834df28e1374c884424f812da65eb
Ángel Quiroz <aquirozdev@gmail.com>
2023-07-25 13:39:52 -0500

    feat: setup tailwind

---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,35 @@
+import colors from "tailwindcss/colors";
+
+const primaryColor = colors.blue[500];
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Add support for dark mode, toggled via a class:
+  // https://tailwindcss.com/docs/dark-mode
+  darkMode: "class",
+  // Inform Tailwind of where our classes will be defined:
+  // https://tailwindcss.com/docs/optimizing-for-production
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        // NOTE: We modify the gray color, as the default Tailwind gray color is heavily saturated
+        // with blue, which makes it look strange in dark mode. This gray color is more balanced,
+        // and works better for sites supporting dark mode.
+        gray: colors.gray,
+        // Add a new "primary" color to all Tailwind utilities, so that we can easily change it.
+        primary: primaryColor,
+      },
+      // Modify the default ring color so that it matches the brand color:
+      ringColor: {
+        DEFAULT: primaryColor,
+      },
+    },
+  },
+  // Add some basic Tailwind plugins to add additional features:
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+  ],
+};
