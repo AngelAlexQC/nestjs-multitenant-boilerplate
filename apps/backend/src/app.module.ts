00b99eec4c99ffc48ad87a03da3785be27e1a52a
Ángel Quiroz <aquirozdev@gmail.com>
2023-07-26 16:54:29 -0500

    feat: setup config

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import * as Joi from 'joi';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      cache: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        DATABASE_URL: Joi.string().required(),
+      }),
+      validationOptions: {
+        abortEarly: true,
+      },
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', '..', 'frontend', 'dist', 'frontend'),
     }),
